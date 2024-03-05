123fc00fea4ce85d4d401c296138ed6c71eb5754
chrisjharvey <97680uk@saeinstitute.edu>
2024-03-05 18:01:06 +0000

    tabs

--- js/index.js
+++ js/index.js
@@ -19,3 +19,22 @@ function navbarLinkClick() {
     navbarToggler.click()
   }
 }
+
+
+const openCity = (evt, cityName) => {
+  // Get all elements with class="tabcontent" and hide them
+  const tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  const tablinks = document.getElementsByClassName("tablinks");
+  for (let i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
